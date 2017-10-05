9e27cf6c8c835c66fd15175fd786a74c05098827
ayushkevich <yushkevich@me.com>
2017-10-05 15:28:34 +0300

    cleanup

--- sample/NetLicensingClient-demo/NetLicensingClient-demo/Properties/AssemblyInfo.cs
+++ sample/NetLicensingClient-demo/NetLicensingClient-demo/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NetLicensingClient-demo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Demo program demonstrates usage of NetLicensingAPI in C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Labs64 GmbH")]
 [assembly: AssemblyProduct("NetLicensingClient-demo")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
